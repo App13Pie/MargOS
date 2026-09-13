5c1a57a66abfe157418d01e99ce65fed90682489
App13Pie <public@app13pie.com>
2026-09-13 13:11:05 +0200

    [feat] remove firefox rpm

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,6 +7,12 @@ cp -avf "/ctx/system_files"/. /
 dnf5 -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-44.noarch.rpm" \
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-44.noarch.rpm"
 
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf5 -y install libva-intel-driver
+
+dnf5 -y remove firefox
+
 dnf5 -y clean all
 
 systemctl enable nvme-sleep-fix.service
